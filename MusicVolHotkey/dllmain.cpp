d2ac9517b3c74ffedd6ba9aa5450817b4860cf03
sekainoue <sekainouexr@gmail.com>
2025-06-07 00:47:39 +0900

    Added numpad and reduced Sleep(200) to Sleep(100)

--- MusicVolHotkey/dllmain.cpp
+++ MusicVolHotkey/dllmain.cpp
@@ -8,7 +8,7 @@ DWORD WINAPI dll_thread(HMODULE hDll) {
             (GetAsyncKeyState('G') & 0x8000)) {
             const auto user_data = sUserData::Get();
 
-            if (GetAsyncKeyState('0') & 0x8000) {
+            if (GetAsyncKeyState('0') & 0x8000 || GetAsyncKeyState(VK_NUMPAD0) & 0x8000) {
                 if (user_data->at<u8>(0x104) != 100) {
                     user_data->at<u8>(0x104) = 100;
                 }
@@ -16,38 +16,38 @@ DWORD WINAPI dll_thread(HMODULE hDll) {
                     user_data->at<u8>(0x104) = 0;
                 }
             }
-            else if (GetAsyncKeyState('9') & 0x8000) {
+            else if (GetAsyncKeyState('9') & 0x8000 || GetAsyncKeyState(VK_NUMPAD9) & 0x8000) {
                 user_data->at<u8>(0x104) = 90;
             }
-            else if (GetAsyncKeyState('8') & 0x8000) {
+            else if (GetAsyncKeyState('8') & 0x8000 || GetAsyncKeyState(VK_NUMPAD8) & 0x8000) {
                 user_data->at<u8>(0x104) = 80;
             }
-            else if (GetAsyncKeyState('7') & 0x8000) {
+            else if (GetAsyncKeyState('7') & 0x8000 || GetAsyncKeyState(VK_NUMPAD7) & 0x8000) {
                 user_data->at<u8>(0x104) = 70;
             }
-            else if (GetAsyncKeyState('6') & 0x8000) {
+            else if (GetAsyncKeyState('6') & 0x8000 || GetAsyncKeyState(VK_NUMPAD6) & 0x8000) {
                 user_data->at<u8>(0x104) = 60;
             }
-            else if (GetAsyncKeyState('5') & 0x8000) {
+            else if (GetAsyncKeyState('5') & 0x8000 || GetAsyncKeyState(VK_NUMPAD5) & 0x8000) {
                 user_data->at<u8>(0x104) = 50;
             }
-            else if (GetAsyncKeyState('4') & 0x8000) {
+            else if (GetAsyncKeyState('4') & 0x8000 || GetAsyncKeyState(VK_NUMPAD4) & 0x8000) {
                 user_data->at<u8>(0x104) = 40;
             }
-            else if (GetAsyncKeyState('3') & 0x8000) {
+            else if (GetAsyncKeyState('3') & 0x8000 || GetAsyncKeyState(VK_NUMPAD3) & 0x8000) {
                 user_data->at<u8>(0x104) = 30;
             }
-            else if (GetAsyncKeyState('2') & 0x8000) {
+            else if (GetAsyncKeyState('2') & 0x8000 || GetAsyncKeyState(VK_NUMPAD2) & 0x8000) {
                 user_data->at<u8>(0x104) = 20;
             }
-            else if (GetAsyncKeyState('1') & 0x8000) {
+            else if (GetAsyncKeyState('1') & 0x8000 || GetAsyncKeyState(VK_NUMPAD1) & 0x8000) {
                 user_data->at<u8>(0x104) = 10;
             }
-            else if (GetAsyncKeyState(VK_OEM_MINUS) & 0x8000) {
+            else if (GetAsyncKeyState(VK_OEM_MINUS) & 0x8000 || GetAsyncKeyState(VK_SUBTRACT) & 0x8000) {
                 user_data->at<u8>(0x104) = 0;
             }
 
-            Sleep(200);
+            Sleep(100);
         }
         Sleep(20);
     }
